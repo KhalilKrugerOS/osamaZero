201d024de46c033a2758062072f1ec813ef99036
khalil lakhoua <arabickhalil@gmail.com>
2023-09-22 17:06:36 +0100

    my first start, the project still unofficial

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const x = 45;
+console.log(Math.floor(x));
+//# sourceMappingURL=index.js.map
---
+++ dist/test.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log(Math.round(44.105));
+//# sourceMappingURL=test.js.map
